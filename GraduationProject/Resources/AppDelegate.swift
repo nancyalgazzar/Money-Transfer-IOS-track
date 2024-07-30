9303a82655af5c776a531ba54587b7084d1f6d13
1234 <a1234@1234s-Mac.local>
2024-07-30 17:33:46 +0300

    check app run

---
+++ GraduationProject/Resources/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  GraduationProject
+//
+//  Created by 1234 on 30/07/2024.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        window  = UIWindow(frame: UIScreen.main.bounds)
+        let SignIn = SignInVC()
+        window?.makeKeyAndVisible()
+        window?.rootViewController = SignIn
+        return true
+    }
+
+
+}
+
