4a21a88406838c3cb28229f74c6e7bbc3b0924ee
1234 <a1234@1234s-Mac.local>
2024-08-06 09:52:31 +0300

    signup api not tested

--- GraduationProject/Networking/SignUpAPIManger.swift
+++ GraduationProject/Networking/SignUpAPIManger.swift
@@ -8,10 +8,10 @@
 import Foundation
 import Alamofire
 class SignUpAPIManager {
-    static func signUp( user: UserModel,completion: @escaping(_ error:Error?, _ status: Bool)->Void ){
+    static func signUp( user: UserModel,completion: @escaping(_ error:Error?, _ status: Bool,_ message: String?)->Void ){
         do {
             let jsonData = try JSONEncoder().encode(user)
-            var request  = URLRequest(url: URL(string: "")!)
+            var request  = URLRequest(url: URL(string: "https://e8c8-156-223-19-253.ngrok-free.app/api/register")!)
             request.httpMethod  = HTTPMethod.post.rawValue
             request.setValue("application/json", forHTTPHeaderField: "Content-Type")
             request.httpBody = jsonData
@@ -19,15 +19,20 @@ class SignUpAPIManager {
                 response in
                 switch response.result {
                 case .success(let value):
-                    print(value as Any)
-                    completion(nil, true)
+                    if let code  = response.response?.statusCode, code == 200{
+                        completion(nil, true,nil)
+                        print(value ?? "cant be printed")//to get token
+                         return
+                    }
+                    completion(nil, false, "Error in data decoding")
+
                 case .failure(let error):
-                    completion(error, false)
+                    completion(error, false, nil)
                 }
             }
         
         } catch  {
-            completion(nil, false)
+            completion(nil, false, "unknown error")
         }
 
     }
--- GraduationProject/Screens/Authentication/SignUp/CompleteUserData/SignUpUserDataEntryViewModel.swift
+++ GraduationProject/Screens/Authentication/SignUp/CompleteUserData/SignUpUserDataEntryViewModel.swift
@@ -8,13 +8,13 @@
 import Foundation
 protocol SignUpUserDataEntryViewModelProtocol: DisplayErrorMessageProtocol {
     var goToHome: (()->())? {get set}
-    func checkDateAndCountryPicking(country: String? , date: String?)
+    func checkDateAndCountryPicking(country: String? , date: String?,nationalID: String?)
 }
 
 class SignUpUserDataEntryViewModel:SignUpUserDataEntryViewModelProtocol {
     var showError: ((String, String) -> Void)?
     var goToHome: (()->())?
-    func checkDateAndCountryPicking(country: String? , date: String?){
+    func checkDateAndCountryPicking(country: String? , date: String?, nationalID: String?){
         
         guard let _ = country else {
             showError?("Sorry", "Please select your country")
@@ -24,13 +24,20 @@ class SignUpUserDataEntryViewModel:SignUpUserDataEntryViewModelProtocol {
             showError?("Sorry", "please enter your birth date")
             return
         }
+        guard let nationalID = nationalID, nationalID.count == 14 else {
+            showError?("Sorry", "Please enter your nationalID")
+            return
+        }
         UserModelManager.user.birthday = date
         UserModelManager.user.nationality = country
-        UserModelManager.user.nationalID = ""
+        UserModelManager.user.nationalID = nationalID
         SignUpAPIManager.signUp(user: UserModelManager.user, completion: {
-            error , status in
+            error , status, message in
             if let error = error {
                 self.showError?("Sorry", error.localizedDescription)
+            }
+            if let message = message {
+                self.showError?("Sorry", message)
             }
             if status {
                 //save token
